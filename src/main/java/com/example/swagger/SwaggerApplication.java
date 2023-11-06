402bdcacae59f82e747cf50ad715a7fe68d9f8a2
BegzodFarmonov <behzodfarmonov3@gmail.com>
2023-11-06 11:05:44 +0500

    yaml_example

--- src/main/java/com/example/swagger/SwaggerApplication.java
+++ src/main/java/com/example/swagger/SwaggerApplication.java
@@ -1,11 +1,12 @@
 package com.example.swagger;
 
+import com.example.swagger.controller.YamlProporties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties()
+@EnableConfigurationProperties(YamlProporties.class)
 public class SwaggerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/swagger/controller/SimpleController.java
+++ src/main/java/com/example/swagger/controller/SimpleController.java
@@ -1,17 +1,32 @@
 package com.example.swagger.controller;
 
+import com.example.swagger.Person;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
-public class
-SimpleController {
+public class SimpleController {
+ private  final YamlProporties yamlProporties;
     @Value("${example.string}")
     private String exampleString;
-    @GetMapping("/exaample")
+    @GetMapping("/example")
     public String example(){
         return exampleString;
     }
+@GetMapping("/exampleLanguages")
+    public List<String> exampleLan(){
+        return yamlProporties.getLanguages();
+    }
+    @GetMapping("/person")
+    public Person exPerson(){
+        return yamlProporties.getPerson();
+    }
+
 }
